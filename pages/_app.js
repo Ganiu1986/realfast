a1b596090ebeded1474dcc3fdca2c90f3e6e7970
Ayinde Ganiu <streemluv4all@mail.com>
2023-04-14 15:57:02 +0100

    my update work

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '@/styles/globals.css';
+import Layouts from '@/components/Layouts';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  )
+}
